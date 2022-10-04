e746c22f0d359eb70c3b4a3556e956b1e9d4c405
milosmilovanovic1988 <swerix@gmail.com>
2022-10-04 18:30:03 +0200

    commit1

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,20 @@
 package pages;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public  abstract  class BasePage {
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+
+    public BasePage() {
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWebDriverWait() {
+        return webDriverWait;
+    }
 }

8c5ae6b57cb2f95e71e1e4f3033bcf072150b64e
milosmilovanovic1988 <swerix@gmail.com>
2022-10-06 20:22:28 +0200

    Added HomePage and LoginPage

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,90 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage extends BasePage {
+
+    private By homeButton = By.xpath("/html/body/div/div[1]/div/header/div/div[3]/a[1]");
+    private By loginButton = By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3]/a[3]");
+    private By signUpButton = By.xpath("/html/body/div/div[1]/div/header/div/div[3]/a[4]");
+    private By languageButton = By.xpath("/html/body/div/div[1]/div/header/div/div[3]/button");
+    private By enLanguage = By.xpath("//*[@id=\"list-item-73\"]");
+    private By esLanguage = By.xpath("//*[@id=\"list-item-75\"]");
+    private By frLanguage = By.xpath("//*[@id=\"list-item-77\"]");
+    private By headerText = By.xpath("/html/body/div/div[1]/main/div/div[2]/div/div[1]/div[1]/h1");
+
+
+    public HomePage(WebDriver driver, WebDriverWait webDriverWait) {
+        super(driver, webDriverWait);
+    }
+    public WebElement getHomeButton() {
+        return getDriver().findElement(homeButton);
+    }
+    public WebElement getLoginButton() {
+        return getDriver().findElement(loginButton);
+    }
+    public WebElement getSignUpButton() {
+        return getDriver().findElement(signUpButton);
+    }
+    public WebElement getLanguageButton() {
+        return getDriver().findElement(languageButton);
+    }
+    public WebElement getEnLanguage() {
+        return getDriver().findElement(enLanguage);
+    }
+    public WebElement getEsLanguage() {
+        return getDriver().findElement(esLanguage);
+    }
+    public WebElement getFrLanguage() {
+        return getDriver().findElement(frLanguage);
+    }
+    public WebElement getHeaderText() {
+        return getDriver().findElement(headerText);
+    }
+    public void clickHomeButton(){
+        getHomeButton().click();
+    }
+    public void clickLoginButton(){
+        getLoginButton().click();
+    }
+    public void clicksignUpButton(){
+        getSignUpButton().click();
+    }
+    public void clicklanguageButton(){
+        getLoginButton().click();
+    }
+    public void clickEnLanguage(){
+        getEnLanguage().click();
+    }
+    public void clickEsLanguage(){
+        getEsLanguage().click();
+    }
+    public void clickFrLanguage(){
+        getFrLanguage().click();
+    }
+    public void waitEn(){
+        getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated(enLanguage));
+    }
+    public void waitFr(){
+        getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated(frLanguage));
+    }
+    public void waitEs(){
+        getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated(esLanguage));
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,59 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage  extends  BasePage{
+
+    private By loginIcon = By.xpath("/html/body/div/div/div/header/div/div[3]/a[3]/span");
+    private By email = By.id("email");
+    private By password = By.id("password");
+    private By logInBtn = By.xpath("/html/body/div/div/main/div/div[2]/div/div/div[3]/span/form/div/div[3]/button/span");
+    private By errorMessage = By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div/div[1]");
+
+
+
+
+    public LoginPage(WebDriver driver, WebDriverWait webDriverWait) {
+        super(driver, webDriverWait);
+    }
+
+    public WebElement  getLoginIcon() {
+        return getDriver().findElement(loginIcon);
+    }
+
+    public WebElement  getEmail() {
+        return getDriver().findElement(email);
+    }
+
+    public WebElement  getPassword() {
+        return getDriver().findElement(password);
+    }
+
+    public WebElement  getLogInBtn() {
+        return getDriver().findElement(logInBtn);
+    }
+
+    public WebElement getErrorMessage() {
+        return getDriver().findElement(errorMessage);
+    }
+
+    public void login(String email, String password){
+        getEmail().clear();
+        getPassword().clear();
+        getEmail().sendKeys(email);
+        getPassword().sendKeys(password);
+        getLogInBtn().click();
+    }
+
+    public void waitForErrorMessage () {
+        getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated(errorMessage));
+    }
+
+
+
+
+}

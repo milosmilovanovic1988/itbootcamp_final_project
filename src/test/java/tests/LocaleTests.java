2301626bdf83e406a6d5c6e18e29bfbec4315393
milosmilovanovic1988 <swerix@gmail.com>
2022-10-09 16:28:17 +0200

    Some changes to tests

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -6,6 +6,12 @@ import org.testng.annotations.Test;
 public class LocaleTests extends  BaseTest{
 
 
+    //Test #3: Set locale to FR
+    //assert:
+    //Postaviti jezik na FR
+    //Verifikovati da se na stranici u hederu javlja tekst Page d'atterrissage
+
+
     @Test
     public void frLanguage() {
         String exRes = "Page d'atterrissag";
@@ -17,7 +23,39 @@ public class LocaleTests extends  BaseTest{
         Assert.assertEquals(acRes,exRes);
     }
 
-  
+    //Test #2: Set locale to EN
+    //assert:
+    //Postaviti jezik na EN
+    //Verifikovati da se na stranici u hederu javlja tekst Landing
+
+    @Test
+    public void enLanguage() {
+        String exRes = "Landing";
+        homePage.clicklanguageButton();
+        homePage.waitEn();
+        homePage.clickEnLanguage();
+        String acRes = homePage.getHeaderText().getText();
+        Assert.assertEquals(acRes, exRes);
+    }
+
+        //Test #1: Set locale to ES
+        //assert:
+        //Postaviti jezik na ES
+        //Verifikovati da se na stranici u hederu javlja tekst Página de aterrizaje
+
+
+        @Test
+        public void esLanguage() {
+            String exRes = "Página de aterrizaje";
+            homePage.clicklanguageButton();
+            homePage.waitEs();
+            homePage.clickEsLanguage();
+            String acRes = homePage.getHeaderText().getText();
+            Assert.assertEquals(acRes, exRes);
+        }
+
+
+
 
 
 }
